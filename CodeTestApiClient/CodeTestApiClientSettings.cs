b5e3be59ea7db40397ef5472a5722e7a52213d79
ecalgarotto <ecalgarotto@msn.com>
2021-11-22 11:07:35 -0300

    Implemente client app

---
+++ CodeTestApiClient/CodeTestApiClientSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swoop.EL.CodeTestApiClient
+{
+    public class CodeTestApiClientSettings
+    {
+        public string Endpoint { get; set; }
+    }
+}
---
+++ CodeTestApiClient/Models/Officer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Swoop.EL.CodeTestApiClient.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class Officer
+    {
+        public string Name { get; set; }
+        public DOB DateOfBirth { get; set; }
+        public int Age { 
+            get
+            {
+                return DateOfBirth!= null ? (DateTime.Now - new DateTime(DateOfBirth.Year, DateOfBirth.Month, DateTime.Now.Day)).Days / 365 : 0;
+            }
+        }
+        public string Role { get; set; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class DOB
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
